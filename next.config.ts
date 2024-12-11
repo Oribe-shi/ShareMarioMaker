254dbf2997d5eb39d1fbeb637d0c73cbf3bfa636
Oribe-shi <nokonoko.mario2@gmail.com>
2024-12-12 07:25:10 +0900

    update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+module.exports = {
+    async headers() {
+        return [
+            {
+                source: "/(.*)", // すべてのリクエストに適用
+                headers: [
+                    {
+                        key: "Content-Security-Policy",
+                        value: `
+                            connect-src 'self' https://discord.com/api/ https://1309879469544050759.discordsays.com;
+                            default-src 'self';
+                            img-src 'self' data: blob:;
+                            script-src 'self';
+                            style-src 'self' 'unsafe-inline';
+                            frame-src 'self';
+                        `
+                            .replace(/\s{2,}/g, " ")
+                            .trim(), // 空白の整形
+                    },
+                ],
+            },
+        ];
+    },
 };
-
-export default nextConfig;
