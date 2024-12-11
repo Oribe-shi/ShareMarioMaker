09aecb61d23c6bf1d36f64b27c442477861fa1c0
Oribe-shi <nokonoko.mario2@gmail.com>
2024-12-12 08:05:53 +0900

    update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,11 @@ module.exports = {
     async headers() {
         return [
             {
-                source: "/(.*)", // 全てのページに適用
+                source: "/(.*)",
                 headers: [
                     {
                         key: "Content-Security-Policy",
-                        value: "connect-src 'self' https://discord.com/api/ https://canary.discord.com/api/ https://ptb.discord.com/api/ https://cdn.discordapp.com/ https://discordsays.com/;",
+                        value: "connect-src 'self' https://1309879469544050759.discordsays.com https://discord.com/api/ https://canary.discord.com/api/ https://ptb.discord.com/api/;",
                     },
                 ],
             },
