3ef5abc88e6b9430bc5ff1c1ef12099ee5e82e5a
Oribe-shi <nokonoko.mario2@gmail.com>
2024-12-12 01:37:48 +0900

    update discordActivty.ts

--- pages/api/discordActivty.ts
+++ pages/api/discordActivty.ts
@@ -17,7 +17,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         }));
 
         res.status(200).json({ participants });
-    } catch (error) {
+    } catch {
         res.status(500).json({ error: "エラーが発生しました" });
     } finally {
         client.destroy();
