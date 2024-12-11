8cd62648a321daccd99dca3037f3af2a90ddcdf8
Oribe-shi <nokonoko.mario2@gmail.com>
2024-12-11 22:16:32 +0900

    update next.config.ts and public/index.html

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,20 @@
-import NextAuth from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
+import type { NextConfig } from "next";
 
-export const authOptions = {
-    providers: [
-        DiscordProvider({
-            clientId: process.env.DISCORD_CLIENT_ID!,
-            clientSecret: process.env.DISCORD_CLIENT_SECRET!,
-        }),
-    ],
+const nextConfig: NextConfig = {
+    reactStrictMode: true, // React の厳密モードを有効にしてパフォーマンスやバグの検出を促進
+    typescript: {
+        ignoreBuildErrors: false, // 本番環境では TypeScript のエラーを無視しない
+    },
+    eslint: {
+        ignoreDuringBuilds: false, // 本番環境で ESLint エラーを無視しない
+    },
+    env: {
+        // .env.local に設定した環境変数を本番環境でも使用するための設定
+        NEXT_PUBLIC_API_TOKEN: process.env.NEXT_PUBLIC_API_TOKEN,
+        DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
+        DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
+        NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    },
 };
 
-export default NextAuth(authOptions);
+export default nextConfig;
